72f872bcee0c832e197f89f759d1c4680a190eb7
konstantin <konstantin.klein@hochfrequenz.de>
2022-01-07 15:53:47 +0100

    ✅More EdifactStack Test Cases, more elaborate Stack Construction (#19)

--- src/maus/reader/mig_reader.py
+++ src/maus/reader/mig_reader.py
@@ -37,9 +37,42 @@ class MigReader(ABC):
             return False
         if (not name_x) and (not name_y):
             return True
-        if name_x.lower() == name_y.lower():  # type:ignore[union-attr]
-            return True
-        return False
+        # neither name_x nor name_y are None below this line
+        return MigReader.make_name_comparable(name_x) == MigReader.make_name_comparable(name_y)  # type:ignore[arg-type]
+
+    @staticmethod
+    def make_name_comparable(orig_str: str) -> str:
+        """
+        Removes all the characters that could be a problem when matching names from the AHB with names from the MIG
+        """
+        result: str = orig_str.lower()
+        for removable_character in [" ", "-", "\n"]:
+            result = result.replace(removable_character, "")
+        return result
+
+    @staticmethod
+    def make_tree_names_comparable(tree: etree.ElementTree) -> None:  # pylint:disable=c-extension-no-member
+        """
+        modifies the provided tree by applying `make_name_comparable` to all name and ahbName attributes
+        """
+        for element in tree.iter():
+            for attrib_key, attrib_value in list(element.attrib.items()):
+                if attrib_key in {"name", "ahbName"}:
+                    element.attrib[attrib_key] = MigXmlReader.make_name_comparable(attrib_value)
+
+    @abstractmethod
+    def get_edifact_stack(
+        self,
+        segment_group_key: str,
+        segment_key: str,
+        data_element_id: str,
+        name: str,
+        #    previous_qualifier: Optional[str] = None,
+    ) -> EdifactStack:
+        """
+        Returns the edifact stack for the given combination of segment group, key, data element and name
+        """
+        raise NotImplementedError("The inheriting class has to implement this method")
 
 
 # pylint:disable=too-few-public-methods
@@ -61,44 +94,54 @@ class MigXmlReader(MigReader):
     """
 
     def __init__(self, init_param: Union[str, Path]):
-        self.root: etree._Element
+        self._original_root: etree._Element
         if isinstance(init_param, str):
-            self.root = etree.fromstring(init_param)
+            self._original_root = etree.fromstring(init_param)
+            self._sanitized_root = etree.fromstring(init_param)
         elif isinstance(init_param, Path):
-            self.root = etree.parse(str(init_param.absolute())).getroot()
+            self._original_root = etree.parse(str(init_param.absolute())).getroot()
+            self._sanitized_root = etree.parse(str(init_param.absolute())).getroot()
         else:
             raise ValueError(f"The type of '{init_param}' is not valid")
-        self.tree = etree.ElementTree(self.root)
+        # the original tree is the unmodified MIG XML Structure with all its quircks
+        self._original_tree: etree.ElementTree = etree.ElementTree(self._original_root)
+        # but turns out it's much easier to handle a sanitized tree that is simplified in a sense that
+        # * has only lower case (ahb)names which are easy to match because they don't contain whitespace,"-" or casing
+        # * has the same structure as the _original_tree so that absolute path expressions from the sanitized tree match
+        self._sanitized_tree: etree.ElementTree = etree.ElementTree(self._sanitized_root)
+        MigReader.make_tree_names_comparable(self._sanitized_tree)
 
     def get_format_name(self) -> str:
         """
         the root element of the XML is the name of the EDIFACT format
         """
-        return self.root.tag
+        return self._original_root.tag
 
-    def element_to_edifact_stack(self, element: etree._Element) -> EdifactStack:
+    def element_to_edifact_stack(self, element: etree.Element, use_sanitized_tree: bool) -> EdifactStack:
         """
         extract the edifact seed path from the given element
         :return:
         """
-        xpath = self.tree.getpath(element)
+        if use_sanitized_tree:
+            xpath = self._sanitized_tree.getpath(element)
+        else:
+            xpath = self._original_tree.getpath(element)
         stack = EdifactStack(levels=[])
         iter_path = "/" + xpath.split("/")[1]
         for leaf in xpath.split("/")[2:]:
             iter_path += "/" + leaf
-            leaf_element = self.root.xpath(iter_path)[0]  # type:ignore[attr-defined]
-            stack.levels.append(
-                EdifactStackLevel(name=leaf_element.attrib["name"], is_groupable=leaf_element.tag == "class")
-            )
+            # here we _always_ need to use the original root!
+            leaf_element = self._original_root.xpath(iter_path)[0]  # type:ignore[attr-defined]
+            level_name: str
+            if "ahbName" in leaf_element.attrib:
+                level_name = leaf_element.attrib["ahbName"]
+            else:
+                level_name = leaf_element.attrib["name"]
+            stack.levels.append(EdifactStackLevel(name=level_name, is_groupable=leaf_element.tag == "class"))
         return stack
 
-    def get_unique_result_by_xpath(self, query_path: str) -> _XQueryPathResult:
-        """
-        Tries to find an element for the given query path.
-        If there's exactly 1 result, it is returned.
-        If there are 0 or >1 results a ValueError is raised.
-        """
-        candidates = list(self.root.xpath(query_path))
+    @staticmethod
+    def _list_to_xquerypathresult(candidates: List[Element]) -> _XQueryPathResult:
         if len(candidates) == 0:
             return _XQueryPathResult(candidates=None, is_unique=None, unique_result=None)
             # the == 1 case is handled last
@@ -106,30 +149,121 @@ class MigXmlReader(MigReader):
             return _XQueryPathResult(candidates=candidates, is_unique=False, unique_result=None)
         return _XQueryPathResult(candidates=None, is_unique=True, unique_result=candidates[0])
 
+    def get_unique_result_by_xpath(self, query_path: str, use_sanitized_tree: bool) -> _XQueryPathResult:
+        """
+        Tries to find an element for the given query path.
+        If there's exactly 1 result, it is returned.
+        If there are 0 or >1 results a ValueError is raised.
+        """
+        candidates: List[Element]
+        if use_sanitized_tree:
+            candidates = list(self._sanitized_root.xpath(query_path))
+        else:
+            candidates = list(self._original_root.xpath(query_path))
+        return MigXmlReader._list_to_xquerypathresult(candidates)
+
+    def get_unique_result_by_segment_group(
+        self, candidates: List[Element], segment_group_key: str, use_sanitized_tree: bool
+    ) -> _XQueryPathResult:
+        """
+        keep those elements that have the correct segment_group_key
+        """
+        filtered = [
+            e
+            for e in candidates
+            if self.get_parent_segment_group_key(e, use_sanitized_tree=use_sanitized_tree) == segment_group_key
+        ]
+        return MigXmlReader._list_to_xquerypathresult(filtered)
+
+    @staticmethod
+    def _get_segment_group_key_or_none(element: Element) -> Optional[str]:
+        """
+        returns the segment group of element if present; None otherwise
+        """
+        if "ref" in element.attrib and element.attrib["ref"].startswith("SG"):
+            # the trivial case
+            return element.attrib["ref"]
+        return None
+
+    def get_parent_segment_group_key(self, element: Element, use_sanitized_tree: bool) -> Optional[str]:
+        """
+        iterate from element towards root and return the first segment group found (the one closes to element).
+        returns None if no segment group was found
+        """
+        segment_group_key = MigXmlReader._get_segment_group_key_or_none(element)
+        if segment_group_key is not None:
+            return segment_group_key
+        if use_sanitized_tree:
+            xpath = self._sanitized_tree.getpath(element)
+        else:
+            xpath = self._original_tree.getpath(element)
+        path_parts = list(xpath.split("/"))
+        sub_paths: List[str] = []
+        for depth in range(2, len(path_parts)):
+            sub_path = "/".join(path_parts[0:depth])
+            sub_paths.append(sub_path)
+        # if xpath was "/foo/bar/asd/xyz", sub_paths is ["/foo", "/foo/bar", "/foo/bar/asd"] now (xyz is not contained!)
+        sub_paths.reverse()
+        for sub_path in sub_paths:
+            leaf_element = self._original_root.xpath(sub_path)[0]  # type:ignore[attr-defined]
+            segment_group_key = MigXmlReader._get_segment_group_key_or_none(leaf_element)
+            if segment_group_key is not None:
+                return segment_group_key
+        return None
+
     # pylint:disable=unused-argument
     def get_edifact_stack(
-        self, segment_group_key: str, segment_key: str, data_element_id: str, name: Optional[str] = None
+        self,
+        segment_group_key: str,
+        segment_key: str,
+        data_element_id: str,
+        name: str,
+        #       previous_qualifier: Optional[str] = None,
     ) -> EdifactStack:
         """
         get the edifact stack for the given segment_group, segment... combination
         :return:
         """
         segment_de_result = self.get_unique_result_by_xpath(
-            f".//*[@meta.id='{data_element_id}' and starts-with(@ref, '{segment_key}')]"
+            f".//*[@meta.id='{data_element_id}' and starts-with(@ref, '{segment_key}')]", use_sanitized_tree=False
         )
         if segment_de_result.is_unique:
-            return self.element_to_edifact_stack(segment_de_result.unique_result)
+            return self.element_to_edifact_stack(segment_de_result.unique_result, use_sanitized_tree=False)
         if segment_de_result.is_unique is False and segment_de_result.candidates is not None:
             filtered_by_names = [
-                x for x in segment_de_result.candidates if MigReader.are_similar_names(x.attrib["name"], name)
+                x
+                for x in segment_de_result.candidates
+                if MigReader.are_similar_names(x.attrib["name"], name)
+                or ("ahbName" in x.attrib and MigReader.are_similar_names(x.attrib["ahbName"], name))
             ]
             if len(filtered_by_names) == 0:
                 # try to find by parents name
                 via_parents_name_result = self.get_unique_result_by_xpath(
-                    f".//class[@name='{name}']/*[@meta.id='{data_element_id}' and starts-with(@ref, '{segment_key}')]"
+                    # pylint:disable=line-too-long
+                    f".//class[@name='{MigReader.make_name_comparable(name)}']/*[@meta.id='{data_element_id}' and starts-with(@ref, '{segment_key}')]",
+                    use_sanitized_tree=True,
                 )
                 if via_parents_name_result.is_unique:
-                    return self.element_to_edifact_stack(via_parents_name_result.unique_result)
+                    return self.element_to_edifact_stack(via_parents_name_result.unique_result, use_sanitized_tree=True)
+                if via_parents_name_result.candidates is None:
+                    via_parents_ahb_name_result = self.get_unique_result_by_xpath(
+                        # pylint:disable=line-too-long
+                        f".//class[@ahbName='{MigReader.make_name_comparable(name)}']/*[@meta.id='{data_element_id}' and starts-with(@ref, '{segment_key}')]",
+                        use_sanitized_tree=True,
+                    )
+                    if via_parents_ahb_name_result.is_unique:
+                        return self.element_to_edifact_stack(
+                            via_parents_ahb_name_result.unique_result, use_sanitized_tree=True
+                        )
+            elif len(filtered_by_names) == 1:
+                return self.element_to_edifact_stack(filtered_by_names[0], use_sanitized_tree=False)
+            else:  # len(filtered_by_names) >1
+                filtered_by_sg = self.get_unique_result_by_segment_group(
+                    filtered_by_names, segment_group_key, use_sanitized_tree=False
+                )
+                if filtered_by_sg.is_unique:
+                    return self.element_to_edifact_stack(filtered_by_sg.unique_result, use_sanitized_tree=False)
+
         raise ValueError("No idea")
 
     def to_segment_group_hierarchy(self) -> SegmentGroupHierarchy:
--- unittests/test_mig_xml_reader.py
+++ unittests/test_mig_xml_reader.py
@@ -2,11 +2,15 @@ from pathlib import Path
 from typing import Optional
 
 import pytest  # type:ignore[import]
+from lxml import etree  # type:ignore[import]
 
 from maus.reader.mig_reader import MigXmlReader
 
 ALL_MIG_XML_FILES = pytest.mark.datafiles(
     "./unittests/migs/FV2204/template_xmls/mscons_1154.xml",
+    "./unittests/migs/FV2204/template_xmls/utilmd_1154.xml",
+    "./unittests/migs/FV2204/template_xmls/utilmd_2380.xml",
+    "./unittests/migs/FV2204/template_xmls/utilmd_7402.xml",
 )
 
 
@@ -34,35 +38,112 @@ class TestMigXmlReader:
             pytest.param("X", "X", True),
             pytest.param("X", "x", True),
             pytest.param("X", "y", False),
+            pytest.param("Gültigkeit, Beginndatum", "Gültigkeit,Beginndatum", True),
+            pytest.param(
+                "Referenz Vorgangsnummer (aus Anfragenachricht)", "Referenz Vorgangsnummer (aus Anfragenachricht)", True
+            ),
         ],
     )
     def test_are_similar_names(self, x: Optional[str], y: Optional[str], expected_result: bool):
         actual = MigXmlReader.are_similar_names(x, y)
         assert actual == expected_result
 
+    def test_make_tree_names_comparable(self):
+        orig_xml = """<?xml version="1.0"?>
+        <hello>
+        <einfield name="Foo "/>
+        <einclass ahbName="Hallo-Welt">
+            <einfield name=" DiGiTaLiSiErUnG" ahbName="(ZuKunFt)"/>
+        </einclass>
+        </hello>
+        """
+        tree = etree.ElementTree(etree.fromstring(orig_xml))
+        MigXmlReader.make_tree_names_comparable(tree)
+        for element in tree.iter():
+            if "name" in element.attrib:
+                assert element.attrib["name"] == MigXmlReader.make_name_comparable(element.attrib["name"])
+            if "ahbName" in element.attrib:
+                assert element.attrib["ahbName"] == MigXmlReader.make_name_comparable(element.attrib["ahbName"])
+
     @ALL_MIG_XML_FILES
     @pytest.mark.parametrize(
-        "segment_group_key, segment_key, data_element_id, name, expected_path",
+        "mig_xml_path, segment_group_key, segment_key, data_element_id, name, previous_qualifier, expected_path",
         [
             pytest.param(
+                "mscons_1154.xml",
                 "SG1",
                 "RFF",
                 "1154",
                 "Prüfidentifikator",
+                None,
                 '$["Dokument"][0]["Nachricht"][0]["Prüfidentifikator"][0]["ID"]',
+                id="mscons pruefi",
+            ),
+            pytest.param(
+                "utilmd_7402.xml",
+                "SG4",
+                "IDE",
+                "7402",
+                "Vorgangsnummer",
+                None,
+                '$["Dokument"][0]["Nachricht"][0]["Vorgang"][0]["Vorgangsnummer"]',
+                id="utilmd vorgangsnummer",
+            ),
+            pytest.param(
+                "utilmd_2380.xml",
+                "root",
+                "DTM",
+                "2380",
+                "Gültigkeit, Beginndatum",
+                None,
+                '$["Dokument"][0]["Nachricht"][0]["Gültigkeit,Beginndatum"]',
+                id="gueltigkeit,beginndatum",
+            ),
+            pytest.param(
+                "utilmd_2380.xml",
+                "SG4",
+                "DTM",
+                "2380",
+                "Bilanzierungsbeginn",
+                None,
+                '$["Dokument"][0]["Nachricht"][0]["Vorgang"][0]["Bilanzierungsbeginn"]',
+                id="utilmd bilanzierungsbeginn",
+            ),
+            pytest.param(
+                "utilmd_1154.xml",
+                "SG6",
+                "RFF",
+                "1154",
+                "Referenz Vorgangsnummer(aus Anfragenachricht)",
+                None,
+                '$["Dokument"][0]["Nachricht"][0]["Vorgang"][0]["Referenz Vorgangsnummer (aus Anfragenachricht)"][0]["Referenz"]',
+                id="referenz aus anfragenachricht",
+            ),
+            pytest.param(
+                "utilmd_1154.xml",
+                "SG8",
+                "RFF",
+                "1154",
+                "Referenz auf die ID einer Messlokation",
+                "Z19",
+                '$["Dokument"][0]["Nachricht"][0]["Vorgang"][0]["Erforderliche OBIS-Daten der Messlokation"][0]["Referenz auf die ID einer Messlokation"]',
+                id="referenz auf melo-id",
             ),
         ],
     )
     def test_simple_paths(
         self,
         datafiles,
+        mig_xml_path: str,
         segment_group_key: str,
         segment_key: str,
         data_element_id: str,
-        name: Optional[str],
+        name: str,
+        previous_qualifier: Optional[str],
         expected_path: str,
     ):
-        reader = MigXmlReader(Path(datafiles) / "mscons_1154.xml")
-        assert reader.get_format_name() == "MSCONS"
-        actual_stack = reader.get_edifact_stack(segment_group_key, segment_key, data_element_id, name)
+        reader = MigXmlReader(Path(datafiles) / mig_xml_path)
+        actual_stack = reader.get_edifact_stack(
+            segment_group_key, segment_key, data_element_id, name  # , previous_qualifier
+        )
         assert actual_stack.to_json_path() == expected_path
