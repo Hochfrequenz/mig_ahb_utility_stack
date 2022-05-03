f47c922f30e93c16cfee65642cd7a799a98d5260
kevin <kevin.krechan@hochfrequenz.de>
2022-05-03 08:11:56 +0200

    💡 Explain autouse=true

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,8 @@ def change_test_dir(request, monkeypatch):
     This fixture makes sure that the test will use the test case folder as their working directory
     So you can use the directory where the test is living as starting point for your file paths.
 
+    `autouse=True` let's run this fixture before each test.
+
     `request` is a built-in pytest fixture
     `fspath`  is the LocalPath to the test module being executed
     `dirname` is the directory of the test module
