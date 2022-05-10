56012a467b7fb809e1a649d11ecca73c8f1535d1
konstantin <konstantin.klein@hochfrequenz.de>
2022-05-10 13:28:24 +0200

    rename the class, too

--- tests/integration_tests/test_kuendigung_msb_maus.py
+++ tests/integration_tests/test_kuendigung_msb_maus.py
@@ -4,7 +4,7 @@ import pytest  # type:ignore[import]
 from helpers import create_maus_and_assert, write_to_file_or_assert_equality  # type:ignore[import]
 
 
-class TestEndeMsbMaus:
+class TestKuendigungMsbMaus:
     """
     A unit test that ensures that the 11039/40/41 MAUS.json are created.
     """
