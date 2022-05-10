0b6c712e3a6017875dc8d63deff2170c847a56a5
konstantin <konstantin.klein@hochfrequenz.de>
2022-05-10 13:42:13 +0200

    Add 11116 Test Case

---
+++ tests/integration_tests/test_stammdatenaenderung_maus.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+import pytest  # type:ignore[import]
+from helpers import create_maus_and_assert, write_to_file_or_assert_equality  # type:ignore[import]
+
+
+class TestStammdatenaenderungMaus:
+    """
+    A unit test that ensures that the 111xx MAUS.json are created.
+    """
+
+    @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2c.template")
+    @pytest.mark.datafiles("./edifact-templates/ahbs/FV2110/UTILMD/11116.csv")
+    @pytest.mark.datafiles("../unit_tests/migs/FV2204/segment_group_hierarchies/sgh_utilmd.json")
+    def test_maus_creation_11116(self, datafiles):
+        create_maus_and_assert(
+            csv_path=Path(datafiles) / "11116.csv",
+            sgh_path=Path(datafiles) / "sgh_utilmd.json",
+            template_path=Path(datafiles) / Path("UTILMD5.2c.template"),
+            maus_path=Path("edifact-templates/maus/FV2110/UTILMD/11116_maus.json"),
+        )
