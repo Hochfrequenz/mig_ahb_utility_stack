10751a70a5d38de71ab95f6ae02b190c25434f62
konstantin <konstantin.klein@hochfrequenz.de>
2022-05-10 13:30:01 +0200

    add integration tests

---
+++ tests/integration_tests/test_ende_msb_maus.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import pytest  # type:ignore[import]
+from helpers import create_maus_and_assert, write_to_file_or_assert_equality  # type:ignore[import]
+
+
+class TestEndeMsbMaus:
+    """
+    A unit test that ensures that the 11051/52/53 MAUS.json are created.
+    """
+
+    @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2c.template")
+    @pytest.mark.datafiles("./edifact-templates/ahbs/FV2110/UTILMD/11051.csv")
+    @pytest.mark.datafiles("../unit_tests/migs/FV2204/segment_group_hierarchies/sgh_utilmd.json")
+    def test_maus_creation_11051(self, datafiles):
+        create_maus_and_assert(
+            csv_path=Path(datafiles) / "11051.csv",
+            sgh_path=Path(datafiles) / "sgh_utilmd.json",
+            template_path=Path(datafiles) / Path("UTILMD5.2c.template"),
+            maus_path=Path("edifact-templates/maus/FV2110/UTILMD/11051_maus.json"),
+        )
+
+    @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2c.template")
+    @pytest.mark.datafiles("./edifact-templates/ahbs/FV2110/UTILMD/11052.csv")
+    @pytest.mark.datafiles("../unit_tests/migs/FV2204/segment_group_hierarchies/sgh_utilmd.json")
+    def test_maus_creation_11052(self, datafiles):
+        create_maus_and_assert(
+            csv_path=Path(datafiles) / "11052.csv",
+            sgh_path=Path(datafiles) / "sgh_utilmd.json",
+            template_path=Path(datafiles) / Path("UTILMD5.2c.template"),
+            maus_path=Path("edifact-templates/maus/FV2110/UTILMD/11052_maus.json"),
+        )
+
+    @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2c.template")
+    @pytest.mark.datafiles("./edifact-templates/ahbs/FV2110/UTILMD/11053.csv")
+    @pytest.mark.datafiles("../unit_tests/migs/FV2204/segment_group_hierarchies/sgh_utilmd.json")
+    def test_maus_creation_11053(self, datafiles):
+        create_maus_and_assert(
+            csv_path=Path(datafiles) / "11053.csv",
+            sgh_path=Path(datafiles) / "sgh_utilmd.json",
+            template_path=Path(datafiles) / Path("UTILMD5.2c.template"),
+            maus_path=Path("edifact-templates/maus/FV2110/UTILMD/11053_maus.json"),
+        )
