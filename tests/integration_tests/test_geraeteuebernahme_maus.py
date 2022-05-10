808cbf622d323bac6cf836117b65e3203db66770
konstantin <konstantin.klein@hochfrequenz.de>
2022-05-10 13:09:28 +0200

    ✔ Add 11057/58/59 Integration Tests (#95)
    * ✔ Add 11057/58/59 Integration Tests
    
    * update submodule

---
+++ tests/integration_tests/test_geraeteuebernahme_maus.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import pytest  # type:ignore[import]
+from helpers import create_maus_and_assert, write_to_file_or_assert_equality  # type:ignore[import]
+
+
+class TestGeraeteuebernahmeMaus:
+    """
+    A unit test that ensures that the 11057/58/59 MAUS.json are created
+    """
+
+    @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2c.template")
+    @pytest.mark.datafiles("./edifact-templates/ahbs/FV2110/UTILMD/11057.csv")
+    @pytest.mark.datafiles("../unit_tests/migs/FV2204/segment_group_hierarchies/sgh_utilmd.json")
+    def test_maus_creation_11057(self, datafiles):
+        create_maus_and_assert(
+            csv_path=Path(datafiles) / "11057.csv",
+            sgh_path=Path(datafiles) / "sgh_utilmd.json",
+            template_path=Path(datafiles) / Path("UTILMD5.2c.template"),
+            maus_path=Path("edifact-templates/maus/FV2110/UTILMD/11057_maus.json"),
+        )
+
+    @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2c.template")
+    @pytest.mark.datafiles("./edifact-templates/ahbs/FV2110/UTILMD/11058.csv")
+    @pytest.mark.datafiles("../unit_tests/migs/FV2204/segment_group_hierarchies/sgh_utilmd.json")
+    def test_maus_creation_11058(self, datafiles):
+        create_maus_and_assert(
+            csv_path=Path(datafiles) / "11058.csv",
+            sgh_path=Path(datafiles) / "sgh_utilmd.json",
+            template_path=Path(datafiles) / Path("UTILMD5.2c.template"),
+            maus_path=Path("edifact-templates/maus/FV2110/UTILMD/11058_maus.json"),
+        )
+
+    @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2c.template")
+    @pytest.mark.datafiles("./edifact-templates/ahbs/FV2110/UTILMD/11059.csv")
+    @pytest.mark.datafiles("../unit_tests/migs/FV2204/segment_group_hierarchies/sgh_utilmd.json")
+    def test_maus_creation_11059(self, datafiles):
+        create_maus_and_assert(
+            csv_path=Path(datafiles) / "11059.csv",
+            sgh_path=Path(datafiles) / "sgh_utilmd.json",
+            template_path=Path(datafiles) / Path("UTILMD5.2c.template"),
+            maus_path=Path("edifact-templates/maus/FV2110/UTILMD/11059_maus.json"),
+        )
