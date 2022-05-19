1319aa7fffbc091abd92a189dac64f60bb51e9fd
konstantin <konstantin.klein@hochfrequenz.de>
2022-05-19 14:17:03 +0200

    besser

--- src/maus/reader/flat_ahb_reader.py
+++ src/maus/reader/flat_ahb_reader.py
@@ -150,9 +150,7 @@ class FlatAhbCsvReader(FlatAhbReader):
         value_pool_entry, description = FlatAhbCsvReader.separate_value_pool_entry_and_name(
             ahb_row["Codes und Qualifier"], ahb_row["Beschreibung"]
         )
-        bedingungen = FlatAhbCsvReader._extract_bedingungen(ahb_row["Bedingung"])
-        for bedingungs_tuple in bedingungen:
-            self.bedingungen.update({bedingungs_tuple[0]: bedingungs_tuple[1]})
+        self.bedingungen.update(FlatAhbCsvReader._extract_bedingungen(ahb_row["Bedingung"]))
         segment_group: Optional[str] = None
         if FlatAhbCsvReader._is_segment_group(ahb_row["Segment Gruppe"]):
             segment_group = ahb_row["Segment Gruppe"]
@@ -232,7 +230,7 @@ class FlatAhbCsvReader(FlatAhbReader):
     _bedingung_pattern = re.compile(r"\[(?P<key>\d+)\]\s*(?P<text>[^\[\]]+)\s*")  # https://regex101.com/r/hN5x9w/1
 
     @staticmethod
-    def _extract_bedingungen(candidate: [Optional[str]]) -> Dict[str, str]:
+    def _extract_bedingungen(candidate: Optional[str]) -> Dict[str, str]:
         """
         Checks if the given candidate is a bedingung. If no, returns empty list.
         If yes returns a list of tuples where foreach tuple the Bedingung key (e.g. "494") is key and the Bedingung +
