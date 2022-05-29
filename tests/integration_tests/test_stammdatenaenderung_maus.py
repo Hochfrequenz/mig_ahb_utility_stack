8b743b4830ad713321f25682f53601de5b0ae9d9
konstantin <konstantin.klein@hochfrequenz.de>
2022-05-29 19:39:38 +0200

    ✔ Add 11124 Integration Test

--- tests/integration_tests/test_stammdatenaenderung_maus.py
+++ tests/integration_tests/test_stammdatenaenderung_maus.py
@@ -20,6 +20,17 @@ class TestStammdatenaenderungMaus:
             maus_path=Path("edifact-templates/maus/FV2110/UTILMD/11116_maus.json"),
         )
 
+    @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2c.template")
+    @pytest.mark.datafiles("./edifact-templates/ahbs/FV2110/UTILMD/11124.csv")
+    @pytest.mark.datafiles("../unit_tests/migs/FV2204/segment_group_hierarchies/sgh_utilmd.json")
+    def test_maus_creation_11124(self, datafiles):
+        create_maus_and_assert(
+            csv_path=Path(datafiles) / "11124.csv",
+            sgh_path=Path(datafiles) / "sgh_utilmd.json",
+            template_path=Path(datafiles) / Path("UTILMD5.2c.template"),
+            maus_path=Path("edifact-templates/maus/FV2110/UTILMD/11124_maus.json"),
+        )
+
     """
     @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2c.template")
     @pytest.mark.datafiles("./edifact-templates/ahbs/FV2110/UTILMD/11117.csv")
