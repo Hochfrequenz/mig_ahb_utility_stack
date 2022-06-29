f788b3f482066658e3130899dee46059835ef537
konstantin <konstantin.klein@hochfrequenz.de>
2022-06-29 15:59:57 +0200

    add 11117 integration test

--- tests/integration_tests/test_stammdatenaenderung_maus.py
+++ tests/integration_tests/test_stammdatenaenderung_maus.py
@@ -43,6 +43,17 @@ class TestStammdatenaenderungMaus:
         )
 
     @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2c.template")
+    @pytest.mark.datafiles("./edifact-templates/ahbs/FV2110/UTILMD/11117.csv")
+    @pytest.mark.datafiles("../unit_tests/migs/FV2204/segment_group_hierarchies/sgh_utilmd.json")
+    def test_maus_creation_11117(self, datafiles):
+        create_maus_and_assert(
+            csv_path=Path(datafiles) / "11117.csv",
+            sgh_path=Path(datafiles) / "sgh_utilmd.json",
+            template_path=Path(datafiles) / Path("UTILMD5.2c.template"),
+            maus_path=Path("edifact-templates/maus/FV2110/UTILMD/11117_maus.json"),
+        )
+
+    @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2c.template")
     @pytest.mark.datafiles("./edifact-templates/ahbs/FV2110/UTILMD/11120.csv")
     @pytest.mark.datafiles("../unit_tests/migs/FV2204/segment_group_hierarchies/sgh_utilmd.json")
     def test_maus_creation_11120(self, datafiles):
