6db2d2c76b07af6b4f71c04c617b8a5568f077fc
konstantin <konstantin.klein@hochfrequenz.de>
2022-09-02 14:10:03 +0200

    remove old test

--- tests/integration_tests/test_stammdatenaenderung_maus.py
+++ tests/integration_tests/test_stammdatenaenderung_maus.py
@@ -224,16 +224,7 @@ class TestStammdatenaenderungMaus:
             maus_path=Path("edifact-templates/maus/FV2110/UTILMD/11170_maus.json"),
         )
 
-    @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2e.template")
-    @pytest.mark.datafiles("./edifact-templates/ahbs/FV2210/UTILMD/11170.csv")
-    @pytest.mark.datafiles("../unit_tests/migs/FV2210/segment_group_hierarchies/sgh_utilmd.json")
-    def test_maus_creation_11170_52e(self, datafiles):
-        create_maus_and_assert(
-            csv_path=Path(datafiles) / "11109.csv",
-            sgh_path=Path(datafiles) / "sgh_utilmd.json",
-            template_path=Path(datafiles) / Path("UTILMD5.2e.template"),
-            maus_path=Path("edifact-templates/maus/FV2210/UTILMD/11170_maus.json"),
-        )
+    # 11170 seems to be gone in FV2210
 
     @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2c.template")
     @pytest.mark.datafiles("./edifact-templates/ahbs/FV2110/UTILMD/11186.csv")
