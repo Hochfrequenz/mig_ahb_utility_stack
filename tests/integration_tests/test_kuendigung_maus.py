56310ea3763c8ad1e5c3bf3421ac1819bd377a7c
konstantin <konstantin.klein@hochfrequenz.de>
2022-09-02 13:46:50 +0200

    Add Integration Test for 11016/17/18 (#146)
    * Add Integration Tests for 11016/17/18
    
    * update submodule
    
    https://github.com/Hochfrequenz/edifact-templates/pull/173
    
    * use submodule main

--- tests/integration_tests/test_kuendigung_maus.py
+++ tests/integration_tests/test_kuendigung_maus.py
@@ -20,6 +20,17 @@ class TestKuendigungMaus:
             maus_path=Path("edifact-templates/maus/FV2110/UTILMD/11016_maus.json"),
         )
 
+    @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2e.template")
+    @pytest.mark.datafiles("./edifact-templates/ahbs/FV2210/UTILMD/11016.csv")
+    @pytest.mark.datafiles("../unit_tests/migs/FV2210/segment_group_hierarchies/sgh_utilmd.json")
+    def test_maus_creation_11016_52e(self, datafiles):
+        create_maus_and_assert(
+            csv_path=Path(datafiles) / "11016.csv",
+            sgh_path=Path(datafiles) / "sgh_utilmd.json",
+            template_path=Path(datafiles) / Path("UTILMD5.2e.template"),
+            maus_path=Path("edifact-templates/maus/FV2210/UTILMD/11016_maus.json"),
+        )
+
     @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2c.template")
     @pytest.mark.datafiles("./edifact-templates/ahbs/FV2110/UTILMD/11017.csv")
     @pytest.mark.datafiles("../unit_tests/migs/FV2204/segment_group_hierarchies/sgh_utilmd.json")
@@ -31,6 +42,17 @@ class TestKuendigungMaus:
             maus_path=Path("edifact-templates/maus/FV2110/UTILMD/11017_maus.json"),
         )
 
+    @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2e.template")
+    @pytest.mark.datafiles("./edifact-templates/ahbs/FV2210/UTILMD/11017.csv")
+    @pytest.mark.datafiles("../unit_tests/migs/FV2210/segment_group_hierarchies/sgh_utilmd.json")
+    def test_maus_creation_11017_52e(self, datafiles):
+        create_maus_and_assert(
+            csv_path=Path(datafiles) / "11017.csv",
+            sgh_path=Path(datafiles) / "sgh_utilmd.json",
+            template_path=Path(datafiles) / Path("UTILMD5.2e.template"),
+            maus_path=Path("edifact-templates/maus/FV2210/UTILMD/11017_maus.json"),
+        )
+
     @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2c.template")
     @pytest.mark.datafiles("./edifact-templates/ahbs/FV2110/UTILMD/11018.csv")
     @pytest.mark.datafiles("../unit_tests/migs/FV2204/segment_group_hierarchies/sgh_utilmd.json")
@@ -40,6 +62,25 @@ class TestKuendigungMaus:
             sgh_path=Path(datafiles) / "sgh_utilmd.json",
             template_path=Path(datafiles) / Path("UTILMD5.2c.template"),
             maus_path=Path("edifact-templates/maus/FV2110/UTILMD/11018_maus.json"),
+        )
+        assert (
+            len(
+                result.maus.find_segments(
+                    segment_predicate=lambda seg: seg.section_name == "Datum des bereits bestaetigten Vertragsendes",
+                )  # the "ae" instead of "ä" is somehow important, we don't wan't an 'ä' for reasons
+            )
+            > 0
+        )  # https://github.com/Hochfrequenz/edifact-templates/pull/163 / ED4FTR-24952
+
+    @pytest.mark.datafiles("./edifact-templates/edi/UTILMD/UTILMD5.2e.template")
+    @pytest.mark.datafiles("./edifact-templates/ahbs/FV2210/UTILMD/11018.csv")
+    @pytest.mark.datafiles("../unit_tests/migs/FV2210/segment_group_hierarchies/sgh_utilmd.json")
+    def test_maus_creation_11018_52e(self, datafiles):
+        result = create_maus_and_assert(
+            csv_path=Path(datafiles) / "11018.csv",
+            sgh_path=Path(datafiles) / "sgh_utilmd.json",
+            template_path=Path(datafiles) / Path("UTILMD5.2e.template"),
+            maus_path=Path("edifact-templates/maus/FV2210/UTILMD/11018_maus.json"),
         )
         assert (
             len(
