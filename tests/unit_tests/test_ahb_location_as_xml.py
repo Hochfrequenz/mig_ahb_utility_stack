e2f080bdc44c6fea6223106a3e945c0f04c2a978
konstantin <konstantin.klein@hochfrequenz.de>
2023-03-28 21:13:04 +0200

    ✨ Serialize AhbLocation as XML

---
+++ tests/unit_tests/test_ahb_location_as_xml.py
@@ -0,0 +1,34 @@
+import pytest  # type:ignore[import]
+
+from maus.navigation import AhbLocation, AhbLocationLayer
+from maus.reader.ahb_location_xml import from_xml_element, to_xml_element
+
+
+class TestAhbLocationAsXml:
+    """
+    Tests the behaviour of the AhbLocation(Layer) can be serialized as XML
+    """
+
+    @pytest.mark.parametrize(
+        "location",
+        [
+            pytest.param(
+                AhbLocation(
+                    layers=[
+                        AhbLocationLayer(
+                            segment_group_key=None, opening_segment_code="UNH", opening_qualifier="UTILMD"
+                        ),
+                        AhbLocationLayer(segment_group_key="SG2", opening_segment_code="NAD", opening_qualifier="MS"),
+                        AhbLocationLayer(segment_group_key="SG3", opening_segment_code="CTA", opening_qualifier="IC"),
+                    ],
+                    segment_code="COM",
+                    data_element_id="3148",
+                    qualifier=None,
+                )
+            )
+        ],
+    )
+    def test_xml_roundtrip(self, location: AhbLocation):
+        xml_element = to_xml_element(location)
+        deserialized_instance = from_xml_element(xml_element)
+        assert location == deserialized_instance
