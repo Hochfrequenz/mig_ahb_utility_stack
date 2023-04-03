4dcbb423efd3f78d4040e8186e95c307b04a6047
konstantin <konstantin.klein@hochfrequenz.de>
2023-04-03 14:34:20 +0200

    gut gut, doch nicht zurückbauen

--- tests/unit_tests/test_tree_to_sgh.py
+++ tests/unit_tests/test_tree_to_sgh.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest  # type:ignore[import]
 
-from maus.reader.tree_to_sgh import check_file_can_be_parsed_as_tree, read_tree
+from maus.reader.tree_to_sgh import check_file_can_be_parsed_as_tree, check_youngest_tree_is_parseable, read_tree
 
 
 class TestTreeToSgh:
@@ -20,6 +20,10 @@ class TestTreeToSgh:
     @pytest.mark.parametrize("filename", ["UTILTS1.1a.tree"])
     def test_check_file_can_be_parsed_as_tree(self, datafiles, filename: str):
         check_file_can_be_parsed_as_tree(Path(datafiles / Path(filename)))
+
+    @pytest.mark.datafiles("./migs/FV2210/segment_group_hierarchies/UTILTS1.1a.tree")
+    def test_check_youngest_tree_is_parseable(self, datafiles):
+        check_youngest_tree_is_parseable(Path(datafiles))
 
     def test_get_edifact_format_error(self):
         something_which_is_not_a_str = 17
