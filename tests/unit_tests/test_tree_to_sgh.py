a14e1676cff5be83685985f7e6da4688d52c50f1
konstantin <konstantin.klein@hochfrequenz.de>
2023-04-03 13:02:31 +0200

    ✨Start parsing `.tree` Files

---
+++ tests/unit_tests/test_tree_to_sgh.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import pytest  # type:ignore[import]
+from maus.reader.tree_to_sgh import read_tree
+
+
+class TestTreeToSgh:
+    """
+    Tests the parser for .tree files
+    """
+
+    @pytest.mark.datafiles("./migs/FV2210/segment_group_hierarchies/UTILTS1.1a.tree")
+    @pytest.mark.parametrize("filename", ["UTILTS1.1a.tree"])
+    def test_get_edifact_format(self, datafiles, filename: str):
+        tree = read_tree(datafiles / Path(filename))
+        assert tree is not None
