b9226b968f8565b1fdc8a6686ee2962803f818c9
konstantin <konstantin.klein@hochfrequenz.de>
2024-07-31 21:26:27 +0200

    Move `EdifactFormat` and `EdifactFormatVersion` to `efoli` package (#434)
    * Move `EdifactFormat` and `EdifactFormatVersion` to `efoli` package
    
    * fix requirements.in
    
    * upgrade efoli
    
    * empty commit
    
    * empty commit
    
    * bump efoli
    
    * empty commit
    
    * isort black

--- src/maus/edifact.py
+++ src/maus/edifact.py
@@ -2,127 +2,14 @@
 This module manages EDIFACT related stuff. It's basically a helper module to avoid stringly typed parameters.
 """
 
-import datetime
 import re
-from enum import Enum
-from typing import Dict, Optional
+from typing import Optional
 
 import attrs
+from efoli import EdifactFormat, EdifactFormatVersion, get_format_of_pruefidentifikator
 
 _PRUEFI_REGEX = r"^[1-9]\d{4}$"
 pruefidentifikator_pattern = re.compile(_PRUEFI_REGEX)
-
-
-# pylint: disable=too-few-public-methods
-class EdifactFormat(str, Enum):
-    """
-    existing EDIFACT formats
-    """
-
-    APERAK = "APERAK"
-    COMDIS = "COMDIS"  #: communication dispute
-    CONTRL = "CONTRL"  #: control messages
-    IFTSTA = "IFTSTA"  #: Multimodaler Statusbericht
-    INSRPT = "INSRPT"  #: Prüfbericht
-    INVOIC = "INVOIC"  #: invoice
-    MSCONS = "MSCONS"  #: meter readings
-    ORDCHG = "ORDCHG"  #: changing an order
-    ORDERS = "ORDERS"  #: orders
-    ORDRSP = "ORDRSP"  #: orders response
-    PRICAT = "PRICAT"  #: price catalogue
-    QUOTES = "QUOTES"  #: quotes
-    REMADV = "REMADV"  #: zahlungsavis
-    REQOTE = "REQOTE"  #: request quote
-    PARTIN = "PARTIN"  #: market partner data
-    UTILMD = "UTILMD"  #: utilities master data
-    UTILMDG = "UTILMDG"  #: utilities master data for 'Gas'
-    UTILMDS = "UTILMDS"  #: utilities master data for 'Strom'
-    UTILMDW = "UTILMDW"  #: utilities master data 'Wasser'
-    UTILTS = "UTILTS"  #: formula
-
-    def __str__(self):
-        return self.value
-
-
-_edifact_mapping: Dict[str, EdifactFormat] = {
-    "99": EdifactFormat.APERAK,
-    "29": EdifactFormat.COMDIS,
-    "21": EdifactFormat.IFTSTA,
-    "23": EdifactFormat.INSRPT,
-    "31": EdifactFormat.INVOIC,
-    "13": EdifactFormat.MSCONS,
-    "39": EdifactFormat.ORDCHG,
-    "17": EdifactFormat.ORDERS,
-    "19": EdifactFormat.ORDRSP,
-    "27": EdifactFormat.PRICAT,
-    "15": EdifactFormat.QUOTES,
-    "33": EdifactFormat.REMADV,
-    "35": EdifactFormat.REQOTE,
-    "37": EdifactFormat.PARTIN,
-    "11": EdifactFormat.UTILMD,
-    "25": EdifactFormat.UTILTS,
-    "91": EdifactFormat.CONTRL,
-    "92": EdifactFormat.APERAK,
-    "44": EdifactFormat.UTILMD,  # UTILMD for GAS since FV2310
-    "55": EdifactFormat.UTILMD,  # UTILMD for STROM since FV2310
-}
-
-
-class EdifactFormatVersion(str, Enum):
-    """
-    One format version refers to the period in which an AHB is valid.
-    """
-
-    FV2104 = "FV2104"  #: valid from 2021-04-01 until 2021-10-01
-    FV2110 = "FV2110"  #: valid from 2021-10-01 until 2022-04-01
-    FV2210 = "FV2210"  #: valid from 2022-10-01 onwards ("MaKo 2022", was 2204 previously)
-    FV2304 = "FV2304"  #: valid from 2023-04-01 onwards
-    FV2310 = "FV2310"  #: valid from 2023-10-01 onwards
-    FV2404 = "FV2404"  #: valid from 2024-04-01 onwards
-    FV2410 = "FV2410"  #: valid from 2024-10-01 onwards
-    FV2504 = "FV2504"  #: valid from 2025-04-04 onwards
-    FV2510 = "FV2510"  #: valid from 2025-10-01 onwards
-    # whenever you add another value here, please also make sure to add its key date to get_edifact_format_version below
-
-    def __str__(self):
-        return self.value
-
-
-def get_edifact_format_version(key_date: datetime.datetime) -> EdifactFormatVersion:
-    """
-    Retrieves the appropriate Edifact format version applicable for the given key date.
-
-    This function determines the correct Edifact format version by comparing the provided key date
-    against a series of predefined datetime thresholds. Each threshold corresponds to a specific
-    version of the Edifact format.
-
-    :param key_date: The date for which the Edifact format version is to be determined.
-    :return: The Edifact format version valid for the specified key date.
-    """
-    format_version_thresholds = [
-        (datetime.datetime(2021, 9, 30, 22, 0, 0, 0, tzinfo=datetime.timezone.utc), EdifactFormatVersion.FV2104),
-        (datetime.datetime(2022, 9, 30, 22, 0, 0, 0, tzinfo=datetime.timezone.utc), EdifactFormatVersion.FV2110),
-        (datetime.datetime(2023, 3, 31, 22, 0, 0, 0, tzinfo=datetime.timezone.utc), EdifactFormatVersion.FV2210),
-        (datetime.datetime(2023, 9, 30, 22, 0, 0, 0, tzinfo=datetime.timezone.utc), EdifactFormatVersion.FV2304),
-        (datetime.datetime(2024, 4, 2, 22, 0, 0, 0, tzinfo=datetime.timezone.utc), EdifactFormatVersion.FV2310),
-        (datetime.datetime(2024, 9, 30, 22, 0, 0, 0, tzinfo=datetime.timezone.utc), EdifactFormatVersion.FV2404),
-        (datetime.datetime(2025, 4, 3, 22, 0, 0, 0, tzinfo=datetime.timezone.utc), EdifactFormatVersion.FV2410),
-        (datetime.datetime(2025, 9, 30, 22, 0, 0, 0, tzinfo=datetime.timezone.utc), EdifactFormatVersion.FV2504),
-    ]
-
-    for threshold_date, version in format_version_thresholds:
-        if key_date < threshold_date:
-            return version
-
-    return EdifactFormatVersion.FV2510
-
-
-def get_current_edifact_format_version() -> EdifactFormatVersion:
-    """
-    returns the edifact_format_version that is valid as of now
-    """
-    tz_aware_now = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
-    return get_edifact_format_version(tz_aware_now)
 
 
 def is_edifact_boilerplate(segment_code: Optional[str]) -> bool:
@@ -132,20 +19,6 @@ def is_edifact_boilerplate(segment_code: Optional[str]) -> bool:
     if not segment_code:
         return True
     return segment_code.strip() in {"UNT", "UNZ"}
-
-
-def get_format_of_pruefidentifikator(pruefidentifikator: str) -> EdifactFormat:
-    """
-    returns the format corresponding to a given pruefi
-    """
-    if not pruefidentifikator:
-        raise ValueError("The pruefidentifikator must not be falsy")
-    if not pruefidentifikator_pattern.match(pruefidentifikator):
-        raise ValueError(f"The pruefidentifikator '{pruefidentifikator}' is invalid.")
-    try:
-        return _edifact_mapping[pruefidentifikator[:2]]
-    except KeyError as key_error:
-        raise ValueError(f"No Edifact format was found for pruefidentifikator '{pruefidentifikator}'.") from key_error
 
 
 # pylint:disable=unused-argument
